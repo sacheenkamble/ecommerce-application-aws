86eeb12e9dd720cd195f7da91640f5ca562d7443
sacheenkamble <50636428+sacheenkamble@users.noreply.github.com>
2025-08-22 23:31:38 +0530

    Update backend.tf

--- project01/awsaccounts/network/vpc-network-01/backend.tf
+++ project01/awsaccounts/network/vpc-network-01/backend.tf
@@ -1,1 +1,18 @@
+##Define terraform version
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.10.0"
+    }
+  }
+}
+
+##Define AWS provider configurations
+provider "aws" {
+  # Configuration options
+}
+
+
+##Define terraform backend for this resource
 
