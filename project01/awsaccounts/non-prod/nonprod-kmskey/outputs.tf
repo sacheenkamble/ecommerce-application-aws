6c5610ea8b65fa90254d331758a8b2167b7f6724
sacheenkamble@gmail.com <sacheenkamble@gmail.com>
2025-09-04 04:19:04 +0000

    update output file

--- project01/awsaccounts/non-prod/nonprod-kmskey/outputs.tf
+++ project01/awsaccounts/non-prod/nonprod-kmskey/outputs.tf
@@ -1,11 +1,11 @@
 # Output AWS KMS Key ID
 output "kms_key_id" {
   value       = module.kms_key.kms_key_id
-  description = "The ID of the AWS-KMS key"
+  description = "The ID of the AWS KMS key"
 }
 
 # Output AWS KMS Key ARN
 output "kms_key_arn" {
   value       = module.kms_key.kms_key_arn
-  description = "The ARN of the AWS-KMS key"
+  description = "The ARN of the AWS KMS key"
 }
